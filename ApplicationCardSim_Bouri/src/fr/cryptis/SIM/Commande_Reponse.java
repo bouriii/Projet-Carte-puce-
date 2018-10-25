6e16613c86e30eccda159972ae170cfc1fe625c8
BOURI Mohamed <bouri_smi@hotmail.com>
2018-10-25 22:15:03 +0200

    correction tmp

--- ApplicationCardSim_Bouri/src/fr/cryptis/SIM/Commande_Reponse.java
+++ ApplicationCardSim_Bouri/src/fr/cryptis/SIM/Commande_Reponse.java
@@ -25,20 +25,14 @@ public class Commande_Reponse {
 	List<CardTerminal> terminal;
 	public static boolean pin;
 	byte [][]cmmd;
-	
-	
 	Commande_Reponse(byte cmmd [][]){
-		
 		tf = TerminalFactory.getDefault();
 		liste = tf.terminals();
 		terminal = null;
 		this.cmmd=cmmd;
 		pin=false;
-		
 	}
-	
 	public void connect_SIM(){
-		
 		// selection du terminal
 		try{
 			terminal = liste.list();
@@ -72,39 +66,28 @@ public class Commande_Reponse {
 	}
 	
 	public String Command_SIM(byte cmd[]) throws CardException{
-		
 		int taille,taille_fichier,taille_enrg,nmbr_enrg=0,length;
 		String comande,rp;
 		boolean booln,bool1=false,SMS=false,FDN=false, ADN=false;
 		byte[] taille_fich=new byte[2],msg;
-		
 		if(cmd[5]==0x6F)
 			if(cmd[6]==0x3C)
 				SMS= true;
-		
 		if(cmd[5]==0x6F)
 			if(cmd[6]==0x3A)
 				ADN= true;
-		
 		if(cmd[5]==0x6F)
 			if(cmd[6]==0x3B)
 				FDN= true;
-		
 		commande= new CommandAPDU(cmd);
 		comande ="\nCommande : "+this.ToString();
 		response= canal.transmit(commande);
 		comande+="\nReponse      : "+this.response_SIM();
-		
 		System.out.println(comande);//+"\nresponse : "+new Util().ByteArrayToHexString(response.getBytes()));
-		
-		
-		
-		
 		length=new Util().calcule_taille_donnée((byte)response.getSW());
 		byte w=(byte)response.getSW();
 		commande= new CommandAPDU(new byte[]{(byte)0xA0,(byte)0xC0,(byte)0x00,(byte)0x00,w});
 		System.out.println("\nle comande test 1 . "+new Util().ByteArrayToHexString(commande.getBytes()));
-		
 		comande+="\n\nCommande : "+this.ToString();
 		response= canal.transmit(commande);
 		if( new analyse().presentation_pin == true){
@@ -120,9 +103,7 @@ public class Commande_Reponse {
 		boolean bool=true;
 		rp=new Util().ByteArrayToHexString(response.getBytes());
 		if( SMS || ADN || FDN ){
-			
 			System.out.println(" test test 2: "+new Util().ByteArrayToHexString(response.getBytes()));
-
 			taille_fich[0]=response.getBytes()[2];
 			taille_fich[1]=response.getBytes()[3];
 			taille_fichier=new Util().calcule_taille_2(taille_fich);
@@ -146,22 +127,18 @@ public class Commande_Reponse {
 	   if(!bool1){
 		if( cmd[1]!= (byte)0x88 )
 			if( bool==true ){
-			
 			// Read BINARY
 			System.out.println(" \n response 11 : "+new Util().ByteArrayToHexString(response.getBytes()));
 			commande= new CommandAPDU(new byte[]{(byte)0xA0,(byte)0xB0,(byte)0x00,(byte)0x00,response.getBytes()[3]});
 			comande+="\n\nCommande : "+this.ToString();
 			response= canal.transmit(commande);
-			
 			comande+="\nReponse      : "+this.response_SIM();
 			bool=false;
 			if(cmd[5] == (byte)0x6f){
 				if(cmd[6]==(byte)0x20)
 					bool=true;
 			}
-			
 			if(bool == true){
-				
 				commande= new CommandAPDU(new byte[]{(byte)0xA0,(byte)0xD6,(byte)0x00,(byte)0x00,(byte)0x09,(byte)0x01,(byte)0x23,(byte)0x45,(byte)0x67,(byte)0x89,(byte)0xAB,(byte)0xCD,(byte)0xEF,(byte)0x00});
 				comande+="\n\nCommande : "+this.ToString();
 				response= canal.transmit(commande);
@@ -173,14 +150,11 @@ public class Commande_Reponse {
 				System.out.println("\n Verification de la reponse  : "+this.response_SIM());
 				comande+="\nReponse     : "+this.response_SIM();
 			}
-			
 		}}else if(SMS){
 			// Read RECORD
 			int i=1;
 			while(i<nmbr_enrg){
-				
 				commande= new CommandAPDU(new byte[]{(byte)0xA0,(byte)0xB2,(byte)i,(byte)0x04,response.getBytes()[length-1]});
-				
 			    comande+="\n\nCommande : "+this.ToString();
 				response= canal.transmit(commande);
 				msg=response.getBytes();
@@ -197,18 +171,13 @@ public class Commande_Reponse {
 				comande+=new analyse().analyse_SMS(rp, msg);
 				//String ttt=new analyse().analyse_SMS(rp, msg); //  test
 				//System.out.println("\n\n SMS : "+ttt);
-				i++;
-				
+				i++
 			}
-			
-			
 		}else if(ADN){
 			// Read RECORD
 			int i=1;
 			while(i<nmbr_enrg){
-				
 				    commande= new CommandAPDU(new byte[]{(byte)0xA0,(byte)0xB2,(byte)i,(byte)0x04,response.getBytes()[length-1]});
-				
 				    comande+="\n  Commande         : "+this.ToString();
 					response= canal.transmit(commande);
 					msg=response.getBytes();
@@ -227,53 +196,38 @@ public class Commande_Reponse {
 					response= canal.transmit(commande);
 					comande+="\n  Reponse              : "+this.ToString_reponse();
 					comande+="\n  Analyse ADN"+new analyse().analyse_ADN(rp, msg);
-					i++;
-				
+					i++;			
 			}
 		}else if(FDN){
 			// Read RECORD
 			int i=1;
 			while(i<nmbr_enrg){
-				commande= new CommandAPDU(new byte[]{(byte)0xA0,(byte)0xB2,(byte)i,(byte)0x04,response.getBytes()[length-1]});
-				
+				commande= new CommandAPDU(new byte[]{(byte)0xA0,(byte)0xB2,(byte)i,(byte)0x04,response.getBytes()[length-1]});		
 			    comande+="\nCommande : "+this.ToString();
 				response= canal.transmit(commande);
-				comande+="\nReponse      : "+this.response_SIM();
-				
+				comande+="\nReponse      : "+this.response_SIM();	
 				commande= new CommandAPDU(cmd);
-				response= canal.transmit(commande);
-				
+				response= canal.transmit(commande);				
 				length=new Util().calcule_taille_donnée((byte)response.getSW());
 				byte ww=(byte)response.getSW();
 				commande= new CommandAPDU(new byte[]{(byte)0xA0,(byte)0xC0,(byte)0x00,(byte)0x00,ww});
 				response= canal.transmit(commande);
-				i++;
-				
+				i++;			
 			}
 		}
 		return comande;
 	}
-	
-	public String ToString(){
-		
+	public String ToString(){		
 		return new Util().ByteArrayToHexString(commande.getBytes()); 
 	}
-	
-	public String ToString_reponse(){
-		
+	public String ToString_reponse(){		
 		return new Util().ByteArrayToHexString(response.getBytes()); 
-	}
-	
-	public String response_SIM(){
-		
-		String rspnse;
-		
-		rspnse=new Util().ByteArrayToHexString(response.getBytes());
-		
-		return rspnse;
-				
-	}
-	
+	}	
+	public String response_SIM(){		
+		String rspnse;		
+		rspnse=new Util().ByteArrayToHexString(response.getBytes());		
+		return rspnse;				
+	}	
 	public byte [] reponse_byte() {
 		return response.getBytes();
 	}
